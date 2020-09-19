d19bb0da85415ea973c13a215bfee01c08e592e0
Fortas Abdeldjalil <dr.fcmam5@gmail.com>
2020-09-19 15:30:56 +0200

    feat: add getAdjacentWilayas

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,11 +1,20 @@
 describe("Le'Bled SDK", () => {
   let data;
+  let lbledSdk;
 
   beforeEach(()=> {
-    data  =require('../data/WilayaList.json');
+    data = require('../data/WilayaList.json');
+    lbledSdk = require('../src/index');
   });
 
   it('data should be present', () => {
     expect( data).toHaveLength(48);
+  });
+
+  it('should export API functions', () => {
+    expect(lbledSdk).toMatchObject({
+      getWilayaList: expect.any(Function),
+      getAdjacentWilayas: expect.any(Function),
+    });
   });
 });
