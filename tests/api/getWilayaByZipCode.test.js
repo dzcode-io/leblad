ac6488cbfde157948a6cc60e0507036a35c3e118
Nasser Abachi <abachinasser@gmail.com>
2020-09-20 21:43:46 +0100

    feat: Add getWilayaByZipCode (#9)
    * fix typo
    
    * feat(getwilayabyzipcode): get the target wilaya by one of its zip codes (postal code)
    
    * fix linting
    
    * perf(getwilayabyzipcode): add wilaya projection to getWilayaByZipCode
    
    * Update src/api/getWilayaByZipCode.js
    
    : Update the function documentation
    
    Co-authored-by: Fortas Abdeldjalil <dr.fcmam5@gmail.com>
    
    * docs(getwilayabyzipcode): add getWilayaByZipCode api documentation
    
    * docs(getwilayabyzipcode): add arguments docummentation
    
    * docs: improve getWilayaByZipCode documentation
    
    Co-authored-by: Fortas Abdeldjalil <dr.fcmam5@gmail.com>
    
    Co-authored-by: Fortas Abdeldjalil <dr.fcmam5@gmail.com>

---
+++ tests/api/getWilayaByZipCode.test.js
@@ -0,0 +1,52 @@
+describe('get wilaya by zip code', () => {
+  const mockData = [{
+    mattricule: 1,
+    name: "Adrar",
+    postalCodes: [
+      1000,
+      1001,
+      1002,
+      1003,
+      1004,
+    ]
+  }, {
+    mattricule: 2,
+    name: "Chlef",
+    postalCodes: [
+      2008,
+      2009,
+      2010,
+      2011,
+      2012,
+    ]
+  }];
+
+  let getWilayaByZipCode;
+
+  beforeEach(() => {
+    getWilayaByZipCode = require('../../src/api/getWilayaByZipCode');
+  });
+
+  it('should return return a curried function that returns the data', () => {
+    const fn = getWilayaByZipCode(mockData);
+
+    expect(typeof fn).toBe('function');
+  });
+
+  it('should return the target wialaya object', () => {
+    let result = getWilayaByZipCode(mockData)(2009);
+    expect(result.mattricule).toEqual(2);
+
+    result = getWilayaByZipCode(mockData)(2012);
+    expect(result.mattricule).toEqual(2);
+
+    result = getWilayaByZipCode(mockData)(2008);
+    expect(result.mattricule).toEqual(2);
+  });
+
+  it('should return undefined if the zip code doesn\'t exists', () => {
+    const result = getWilayaByZipCode(mockData)(99999);
+
+    expect(result).toBeUndefined();
+  });
+});
