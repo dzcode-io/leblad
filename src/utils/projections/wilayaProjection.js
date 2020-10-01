f46ca1a99babc70cbf0b7e5e1c903a6f8b3c3740
unknown <jadenhou19@gmail.com>
2020-10-01 10:18:41 -0400

    fixed most eslint

--- src/utils/projections/wilayaProjection.js
+++ src/utils/projections/wilayaProjection.js
@@ -1,6 +1,6 @@
 const _projectWilayaObject = (wilayaObject, attributes) => {
-  return attributes.reduce((acc, attr)=> {
-    return {...acc, [attr]: wilayaObject[attr]};
+  return attributes.reduce((acc, attr) => {
+    return { ...acc, [attr]: wilayaObject[attr] };
   }, {});
 };
 
@@ -18,71 +18,72 @@ const projectWilaya = (wilayaData, projection) => {
   }
   if (Array.isArray(projection)) {
     if (!Array.isArray(wilayaData)) {
-        const data = {}
-        for (let i=0; i < projection.length; i++) {
-            const field = projection[i]
-            if (field.includes('.')) {
-                const splitFields = projection.split('.')
-                let value = wilayaData[splitFields[0]];
-                for (let b=1; b < splitFields.length; b++) {
-                    if (Array.isArray(value)) {
-                        const findValues = []
-                        value.forEach(item => { 
-                            if (value[splitFields[b]] !== null) findValues.push(item[splitFields[b]])
-                        })
-                        value = []
-                        findValues.forEach(item => {
-                            value.push({ [splitFields[b]]: item})
-                        })
-                    }
-                    else {
-                        value = value[splitFields[b]]
-                    }
-                }
-                data[splitFields[0]] = value
+      const data = {};
+      for (let i = 0; i < projection.length; i += 1) {
+        const field = projection[i];
+        if (field.includes('.')) {
+          const splitFields = projection.split('.');
+          let value = wilayaData[splitFields[0]];
+          for (let b = 1; b < splitFields.length; b += 1) {
+            if (Array.isArray(value)) {
+              const findValues = [];
+              value.forEach(item => {
+                if (value[splitFields[b]] !== null) findValues.push(item[splitFields[b]]);
+              });
+              value = [];
+              findValues.forEach(item => {
+                value.push({ [splitFields[b]]: item });
+              });
             }
             else {
-                data[field] = wilayaData[field]
+              value = value[splitFields[b]];
             }
+          }
+          data[splitFields[0]] = value;
         }
-        return data
-    } else if (Array.isArray(wilayaData)) {
-        const final = []
-        wilayaData.forEach(val => {
-            const data = {}
-            for (let i=0; i < projection.length; i++) {
-                const field = projection[i]
-                
-                if (field.includes('.')) {
-                    const splitFields = field.split('.')
-                    let value = val[splitFields[0]];
-                    for (let b=1; b < splitFields.length; b++) {
-                        if (Array.isArray(value)) {
-                            const findValues = []
-                            value.forEach(item => { 
-                                if (value[splitFields[b]] !== null) findValues.push(item[splitFields[b]])
-                            })
-                            value = []
-                            findValues.forEach(item => {
-                                value.push({ [splitFields[b]]: item})
-                            })
-                        }
-                        else {
-                            value = value[splitFields[b]]
-                        }
-                    }
-                    data[splitFields[0]] = value
-                }
-                else {
-                    data[field] = val[field]
-                }
+        else {
+          data[field] = wilayaData[field];
+        }
+      }
+      return data;
+    }
+    if (Array.isArray(wilayaData)) {
+      const final = [];
+      wilayaData.forEach(val => {
+        const data = {};
+        for (let i = 0; i < projection.length; i += 1) {
+          const field = projection[i];
+
+          if (field.includes('.')) {
+            const splitFields = field.split('.');
+            let value = val[splitFields[0]];
+            for (let b = 1; b < splitFields.length; b += 1) {
+              if (Array.isArray(value)) {
+                const findValues = [];
+                value.forEach(item => {
+                  if (value[splitFields[b]] !== null) findValues.push(item[splitFields[b]]);
+                });
+                value = [];
+                findValues.forEach(item => {
+                  value.push({ [splitFields[b]]: item });
+                });
+              }
+              else {
+                value = value[splitFields[b]];
+              }
             }
-            final.push(data)
-        })
-        return final
-       
-    } 
-}
+            data[splitFields[0]] = value;
+          }
+          else {
+            data[field] = val[field];
+          }
+        }
+        final.push(data);
+      });
+      return final;
+
+    }
+  }
   if (Array.isArray(wilayaData)) {
     return wilayaData.map(w => _projectWilayaObject(w, projection));
   }
