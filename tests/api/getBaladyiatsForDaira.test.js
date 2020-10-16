dddc6ed312b9078cf78beef0b2e30c01bb674c82
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-10-16 21:38:35 +0100

    Fix linter errors

--- tests/api/getBaladyiatsForDaira.test.js
+++ tests/api/getBaladyiatsForDaira.test.js
@@ -63,8 +63,8 @@ describe('Get baladyiats for a daira', ()=>{
   it('should return null if the there is no daira', () => {
     expect(getBaladyiatsForDaira(mockData)()).toBeNull();
   });
-  
-    it('should return null if the daira has no baladiats', () => {
+
+  it('should return null if the daira has no baladiats', () => {
     expect(getBaladyiatsForDaira(DAIRA_WITHOUT_BALADIATS)("ORAN")).toBeNull();
   });
 
