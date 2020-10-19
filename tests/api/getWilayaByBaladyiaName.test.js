5375b8c2d39246bfae5fbf29c10d1a3b765fdff0
Redaloukil <loukil.red@gmail.com>
2020-10-20 00:05:57 +0200

    review fixes , redefined some tests

--- tests/api/getWilayaByBaladyiaName.test.js
+++ tests/api/getWilayaByBaladyiaName.test.js
@@ -20,68 +20,7 @@ describe('get matching wilaya', ()=> {
           }
         ]
       },
-      {
-        code: 4702,
-        name: "EL MENIA",
-        name_ar: "المنيعة",
-        name_en: "EL MENIA",
-        baladyiats: [
-          {
-            code: 4702,
-            name: "EL MENIAA",
-            name_en: "EL MENIAA",
-            name_ar: "المنيعة"
-          },
-        ]
-      },
-      {
-        code: 4702,
-        name: "EL MENIA (wilaya déléguée)",
-        name_ar: "المنيعة (ولاية منتدبة)",
-        name_en: "EL MENIA (wilaya déléguée)",
-        baladyiats: [
-          {
-            code: 4702,
-            name: "EL MENIAA",
-            name_en: "EL MENIAA",
-            name_ar: "المنيعة"
-          },
-          {
-            code: 4712,
-            name: "HASSI GARA",
-            name_en: "HASSI GARA",
-            name_ar: "حاسي القارة"
-          }
-        ]
-      },
-      {
-        code: 4703,
-        name: "DHAYET BEN DHAHOUA",
-        name_ar: "ضاية بن ضحوة",
-        name_en: "DHAYET BEN DHAHOUA",
-        baladyiats: [
-          {
-            code: 4703,
-            name: "DHAYET BENDHAHOUA",
-            name_en: "DHAYET BENDHAHOUA",
-            name_ar: "ضاية بن ضحوة"
-          }
-        ]
-      },
-      {
-        code: 4704,
-        name: "BERRIANE",
-        name_ar: "بريان",
-        name_en: "BERRIANE",
-        baladyiats: [
-          {
-            code: 4704,
-            name: "BERRIANE",
-            name_en: "BERRIANE",
-            name_ar: "بريان"
-          }
-        ]
-      }]
+    ]
   }];
 
   const noBaladyiaMockData = [
@@ -128,43 +67,27 @@ describe('get matching wilaya', ()=> {
     expect(typeof fn).toBe('function');
   });
 
-  it('should return null if the wilaya with the given baladyia is not found', () => {
-    const result = getWilayaByBaladyiaName(mockData)("foo");
-    expect(result).toBeNull();
-  });
-  it('should return null if the wilaya with the given baladyia invalid name', () => {
+it('should return null if the wilaya with the given baladyia invalid name', () => {
     const result = getWilayaByBaladyiaName(mockData)("fo");
 
     expect(result).toBeNull();
   });
 
-  it('should return null if there\'s no daira', () => {
-    const result = getWilayaByBaladyiaName(mockData)();
+  it('should return matching wilaya object with name', () => {
+    const result = getWilayaByBaladyiaName(mockData)("GHARDAIA");
 
-    expect(result).toBeNull();
+    expect(result).toEqual(mockData[0]);
   });
 
-  it('should return null if the wilaya with the given daira is empty', () => {
-    const result = getWilayaByBaladyiaName(mockData)("ZRZR");
+it('should return matching wilaya object with arabic name', () => {
+    const result = getWilayaByBaladyiaName(mockData)("غرداية");
 
-    expect(result).toBeNull();
-  });
-
-  it('should return matching wilaya object with arabic name', () => {
-    const result = getWilayaByBaladyiaName(mockData)("GHARDAIA", ["mattricule", "name"]);
-
-    expect(result).toEqual({
-      mattricule: 47,
-      name: "Ghardaïa",
-    });
+    expect(result).toEqual(mockData[0]);
   });
 
   it('should return matching wilaya object with english name', () => {
-    const result = getWilayaByBaladyiaName(mockData)("EL MENIAA", ["mattricule", "name"]);
-    expect(result).toEqual({
-      mattricule: 47,
-      name: "Ghardaïa",
-    });
+    const result = getWilayaByBaladyiaName(mockData)("GHARDAIA");
+    expect(result).toEqual(mockData[0]);
   });
 
   it('should return matching wilaya object with baladyia arabe name', () => {
@@ -173,10 +96,8 @@ describe('get matching wilaya', ()=> {
   });
 
   it('should return matching wilaya object with baladyia arabe name', () => {
-    const result = getWilayaByBaladyiaName(noBaladyiaMockData)("المنيعة", ["mattricule"]);
-    expect(result).toEqual({
-      mattricule: 12,
-    });
+    const result = getWilayaByBaladyiaName(noBaladyiaMockData)("المنيعة");
+    expect(result).toEqual(noBaladyiaMockData[0]);
   });
 });
 
