41600fcb66ec23a3750cc474744abeee299817cc
Fortas Oussama Ilyes <fortasoussamailyes@gmail.com>
2020-10-31 03:16:45 +0100

    refactor(utils): use hasName

--- src/api/getBaladyiatsForDaira.js
+++ src/api/getBaladyiatsForDaira.js
@@ -1,38 +1,32 @@
+const _hasName = require('../utils/hasName');
+
 const MIN_DAIRA_NAME_LENGTH = 3;
 
 const getBaladyiatsForDaira = data =>
 /**
- * Takes a wilaya daira name and returns its baladyiats
- *
- * @example get baladyiats for daira
- * getBaladyiatsForDaira("ORAN")
- *
- * @param { String } daira The Wilaya's "daira"
- * @returns { object[] | null } Returns All baladyiats for daira, or null
- */
+   * Takes a wilaya daira name and returns its baladyiats
+   *
+   * @example get baladyiats for daira
+   * getBaladyiatsForDaira("ORAN")
+   *
+   * @param { String } daira The Wilaya's "daira"
+   * @returns { object[] | null } Returns All baladyiats for daira, or null
+   */
 
-  (daira) => {
+  daira => {
     if (!daira || daira.trim().length < MIN_DAIRA_NAME_LENGTH) {
       return null;
     }
 
-    const dairaName = daira.toLowerCase();
+    const hasGivenDairaName = _hasName(daira.toLowerCase());
 
     const wilaya = data.find(w =>
-      w.dairats.find(d =>
-        d.name.toLowerCase() === dairaName
-      || d.name_en.toLowerCase() === dairaName
-      || d.name_ar === dairaName
-      )
+      w.dairats.find(d => hasGivenDairaName(d))
     );
 
-    if(!wilaya) return null;
+    if (!wilaya) return null;
 
-    const { baladyiats } = wilaya.dairats.find(dn =>
-      dn.name.toLowerCase() === dairaName
-    || dn.name_en.toLowerCase() === dairaName
-    || dn.name_ar === dairaName
-    );
+    const { baladyiats } = wilaya.dairats.find(dn => hasGivenDairaName(dn));
 
     return baladyiats || null;
   };
--- src/api/getWilayaByDairaName.js
+++ src/api/getWilayaByDairaName.js
@@ -1,32 +1,29 @@
 const projectWilaya = require('../utils/projections/projectObject');
+const _hasName = require('../utils/hasName');
 
 const MIN_DAIRA_LENGTH = 3;
 
 const getWilayaByDairaName = data =>
-/**
- * Takes a wilaya daira-name and returns its respective wilaya
- *
- * @example <p> Get Relizane wilaya by 'OUED RHIOU' daira
- * //returns {name: "Relizane", ...}
- * getWilayaByDairaName("")
- *
- * @param { String } daira The Wilaya's daira name(en | ar | fr)
- * @param {String[]} projection a list of  wilaya object attributes to keep
- * @returns { Object | null } Returns matching wilaya, or null
- */
-  (daira, projection) =>  {
+  /**
+   * Takes a wilaya daira-name and returns its respective wilaya
+   *
+   * @example <p> Get Relizane wilaya by 'OUED RHIOU' daira
+   * //returns {name: "Relizane", ...}
+   * getWilayaByDairaName("")
+   *
+   * @param { String } daira The Wilaya's daira name(en | ar | fr)
+   * @param {String[]} projection a list of  wilaya object attributes to keep
+   * @returns { Object | null } Returns matching wilaya, or null
+   */
+  (daira, projection) => {
     if (!daira || daira.trim().length < MIN_DAIRA_LENGTH) {
       return null;
     }
 
-    const dairaName = daira.toLowerCase();
+    const hasGivenDairaName = _hasName(daira.toLowerCase());
 
     const wilaya = data.find(w =>
-      w.dairats.find(d =>
-        d.name.toLowerCase() === dairaName
-      || d.name_en.toLowerCase() === dairaName
-      || d.name_ar === dairaName
-      )
+      w.dairats.find(d => hasGivenDairaName(d))
     );
 
     return projectWilaya(wilaya, projection);
