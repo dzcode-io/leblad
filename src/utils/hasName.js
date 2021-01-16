0a851cd22d39752a5d22f177df50be23235c8013
Abdeldjalil Fortas <dr.fcmam5@gmail.com>
2021-01-16 22:55:36 +0100

    chore: bump dependencies; fix linting

--- src/utils/hasName.js
+++ src/utils/hasName.js
@@ -1,6 +1,6 @@
-const hasName = givenName => {
+const hasName = givenName =>
   // eslint-disable-next-line camelcase
-  return ({ name, name_ar, name_en, name_ber }) => name.toLowerCase() === givenName || name_ar === givenName || name_en.toLowerCase() === givenName || name_ber === givenName;
-};
+  ({ name, name_ar, name_en, name_ber }) => name.toLowerCase() === givenName || name_ar === givenName || name_en.toLowerCase() === givenName || name_ber === givenName
+;
 
 module.exports = hasName;
