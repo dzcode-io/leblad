9a2fcf1ab8c33ce5fdd22920d6d516847daeea27
abderrahmane <abderrahmaneMustapha030898@gmail.com>
2021-06-07 01:09:49 +0100

    feat: add prjections  for get baldiyats by code for a daira and a wilaya

--- src/api/getBaldiyatsForDairaByCodeForWilayaByCode.js
+++ src/api/getBaldiyatsForDairaByCodeForWilayaByCode.js
@@ -1,3 +1,5 @@
+const projectBaladiya = require('../utils/projections/projectObject');
+
 const getBaldiyatsForDairaByCodeForWilayaByCode = (data) =>
 /**
    * Takes a wilaya code (mattricule), daira code (code) and returns array of Baladiyates of wilaya.
@@ -9,17 +11,17 @@ const getBaldiyatsForDairaByCodeForWilayaByCode = (data) =>
    *
    * @param { Number } mattricule wilaya code (mattricule)
    * @param { Number } code baladiya code (code)
-   * @param { String[] } projection a list of Baladyia object attributes to keep
+   * @param { String[] } projections a list of Baladyia object attributes to keep
    * @returns { Object[] | null } list of all baladiyas for wilaya
    */
 
-  (mattricule, code) => {
+  (mattricule, code, projections) => {
     const wilaya = data.find((w) => w.mattricule === mattricule);
     if (wilaya) {
       const daira = wilaya.dairats.find((d) => d.code === code);
 
       if(daira){
-        return daira.baladyiats;
+        return  projectBaladiya(daira.baladyiats, projections);
       }
 
     }
